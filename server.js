648b49aa073ab36ed3374838db190653a1a795e2
Muhinthan <m.tharmendran@gmail.com>
2016-09-20 22:56:37 +0000

    Working but ugly

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 var express = require('express');
 var multer = require('multer');
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, '/uploads');
+    destination: function (req, file, callback) {
+        callback(null, './uploads');
     }
 });
 var upload = multer({ storage: storage });
-//var mongo = require('mongodb').MongoClient;
 
 var app = express();
 
 app.use('/', express.static(__dirname + '/styles'));
 
-app.post('/', upload.single('file'), function(req,res){
-    console.log(req.file);
-    res.send('Hello World.');
+app.post('/analyze', upload.single('fileSize'), function(req,res){
+    res.json({ filesize: req.file.size});
 });
 
 app.listen(8080, function() {
