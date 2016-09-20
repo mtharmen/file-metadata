1893785dc8d183d85add0b86131c2e196a03ee92
Muhinthan <m.tharmendran@gmail.com>
2016-09-20 20:52:34 +0000

    Can't Upload

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var express = require('express');
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '/uploads');
+    }
+});
+var upload = multer({ storage: storage });
 //var mongo = require('mongodb').MongoClient;
 
 var app = express();
 
 app.use('/', express.static(__dirname + '/styles'));
 
-app.get('/api/imagesearch/:terms', function(req, response){
-    
+app.post('/', upload.single('file'), function(req,res){
+    console.log(req.file);
+    res.send('Hello World.');
 });
 
 app.listen(8080, function() {
