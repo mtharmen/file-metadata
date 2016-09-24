9d63e4e20ad179121f020a48a640230f5b8173e0
mtharmen <m.tharmendran@gmail.com>
2016-09-24 14:22:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var multer = require('multer');
+var fs = require('fs');
 var storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null, './uploads');
+        callback(null, './temp');
     }
 });
 var upload = multer({ storage: storage });
@@ -12,6 +13,7 @@ var app = express();
 app.use('/', express.static(__dirname + '/styles'));
 
 app.post('/analyze', upload.single('fileSize'), function(req,res){
+    fs.unlinkSync("./temp/" + req.file.filename);
     res.json({ filesize: req.file.size});
 });
 
